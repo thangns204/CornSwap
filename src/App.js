c46c7cc4ca9769b81578c7b99011d984e37136be
Thắng <thangns204@gmail.com>
2020-12-13 16:14:49 +0700

    structure project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
-import './App.css';
+import React, {useEffect, useState} from 'react';
 
-export const App = () => <p> Hello  </p>
+
+export const App = () => {
+	// Ham useState nhan vao mot tham so la gia tri ban dau
+	// cua state , tra ve mot cap [value, handler] chua gia tri
+	// current state, va handler de change value this state
+	const [storyIds, setStoryIds] = useState([]);
+
+	// Similar   componentDidMount and componentDidUpdate
+	useEffect(() => {
+		setStoryIds('hello');
+	}, []);
+	return (
+		<p> {storyIds} </p>
+	)
+}
+
+// LifeCycle component in react
+//Initialization: setup props and state
+//Mounting: componentWillMount -> render -> componentDidMount
+//Update: componentWillReceiveProps -> shouldComponentUpdate -> componentWillUpdate -> render -> componentDidUpdate
+		// stateComponentUpdate -> ComponentWillUpdate -> render -> componentDidUpdate
+//Unmounting: componentWillUnmount
+
 
 
